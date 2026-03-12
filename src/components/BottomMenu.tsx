fa2f9b15e1fbec00e97e745f58684c6a3b98ed19
Jon Crabtree <156045562+LongNEKK@users.noreply.github.com>
2026-03-12 11:20:18 -1000

    final commit

---
+++ src/components/BottomMenu.tsx
@@ -0,0 +1,44 @@
+import {  Button, Container, Image, Row,Col} from "react-bootstrap";
+import { Facebook, Instagram, Linkedin, Twitter } from "react-bootstrap-icons";
+
+const BottomMenu = () => (
+  <footer id="bottomMenu" className="footer mt-auto py-3 bg-dark">
+    <Container>
+      <Row>
+        <Col>
+          <Image src="/maui-cropped-Logo-1.png" alt="Maui Brewing Co. Logo"  width={175} />
+        </Col>
+        <Col>
+          <h3>MAUI</h3>
+          <br/>
+          KIHEI<br/>
+          KA&apos;ANAPALI
+        </Col>
+        <Col>
+          <h3>OAHU</h3>
+          <br/>
+          KILUA<br/>
+          WAIKIKI
+        </Col>
+        <Col>
+          BREWERY
+          <br/>
+          CONTACT US<br/>
+          GIFT CARDS
+        </Col>
+        <Col>
+          NEWSLETTER
+          <br/>
+          <input type="text" placeholder="Enter Email"/>
+          <Button>SUBMIT</Button> <br/>
+          <Facebook size={35} className="mx-2 my-2"/>
+          <Twitter size={35} className="mx-2 my-2"/>
+          <Instagram size={35} className="mx-2 my-2"/>
+          <Linkedin size={35} className="mx-2 my-2"/>
+        </Col>
+      </Row>
+    </Container>
+  </footer>
+);
+
+export default BottomMenu;
